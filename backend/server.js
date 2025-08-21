2fe7119bd357c56aa780ff8934ddb34d484f9958
SuperLoveDev <adenihikader@gmail.com>
2025-08-21 17:18:30 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ const port = process.env.PORT || 4000;
 app.use(express.json());
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: process.env.FRONTEND_URL || "https://wazo-frontend.onrender.com",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
